478f99647734d4458d9baf58bb0158a9e6e74e2d
anirban-s <baban.chrome@gmail.com>
2020-04-18 04:11:44 +0530

    commiting the updated code with PIL package

--- jpg-png-converter/JPGtoPNGconverter.py
+++ jpg-png-converter/JPGtoPNGconverter.py
@@ -2,3 +2,19 @@ import sys
 import os
 from PIL import Image
 
+# garb the path from command prompt
+jpg_path = sys.argv[1]
+png_path = sys.argv[2]
+
+# if new path does not exist, create new folder
+if os.path.exists(png_path) == False:
+	os.mkdir(png_path)
+
+for filename in os.listdir(jpg_path):
+	img = Image.open(f'{jpg_path}{filename}')
+	clean_name = os.path.splitext(filename)[0]
+	if not os.path.exists(f'{clean_name}.png'):
+		img.save(f'{png_path}{clean_name}.png', 'png')
+
+		
+print('All done!!')
