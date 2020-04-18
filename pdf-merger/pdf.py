a0ecf01333b74e0034f7f089e0d789805f62c52e
anirban-s <baban.chrome@gmail.com>
2020-04-19 03:13:01 +0530

    commiting pdf mergercode

---
+++ pdf-merger/pdf.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import PyPDF2
+
+input  = sys.argv[1:]
+
+def pdf_combiner(pdf_list):
+	merger = PyPDF2.PdfFileMerger()
+	for pdf in pdf_list:
+		merger.append(pdf)
+
+	merger.write('merged.pdf')
+
+pdf_combiner(input)
