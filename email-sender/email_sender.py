4e9da16c6065dd2515ee9c82ec4e2e001332f9ac
anirban-s <baban.chrome@gmail.com>
2020-04-20 17:25:23 +0530

    passwordchecker code added

--- email-sender/email_sender.py
+++ email-sender/email_sender.py
@@ -7,7 +7,7 @@ html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Anirban Sarkar'
-email['to'] = 'codingtutorial101@gmail.com'
+email['to'] = 'baban.chrome@gmail.com'
 email['subject'] = 'Test Email'
 
 email.set_content(html.substitute({'name': 'Tintin'}), 'html')
@@ -15,6 +15,6 @@ email.set_content(html.substitute({'name': 'Tintin'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port = 587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('codingtutorial101@gmail.com', 'password')
+	smtp.login('codingtutorial101@gmail.com', '')
 	smtp.send_message(email)
 	print('All Good!!')
---
+++ passwordchecker/README.md
@@ -0,0 +1,3 @@
+## Run the sctipt with below command
+ `python passwordcheck.py hello test password123`
+ 
